cae50f34fcf9731230c42589f58f5102625106f0
Alex Tappin <alextappin@gmail.com>
2020-04-18 04:05:22 -0700

    finish markup and css

--- src/components/Bag/Bag.jsx
+++ src/components/Bag/Bag.jsx
@@ -3,7 +3,11 @@ import './Bag.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCarSide } from '@fortawesome/free-solid-svg-icons'
 import { faClock } from '@fortawesome/free-solid-svg-icons'
+import { faLongArrowAltRight } from '@fortawesome/free-solid-svg-icons'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
 import ProductCard from '../ProductCard/ProductCard'
+import PaypalLogo from '../../img/payPal.svg'
+
 
 const Bag = ({
   items,
@@ -22,38 +26,50 @@ const Bag = ({
   return (
     <>
     <div className='bag'>
-      <div>
+      <div className='items-side'>
         <h1>YOUR BAG</h1>
         <div>TOTAL: ({itemsLength} {itemsLength != 1 ? 'items' : 'item'}) <b>${totalPrice}</b></div>
         <br></br>
         {items && items.map((item, i) => <ProductCard {...item} key={i}/>)}
-        <div>
-          <button>CHECKOUT</button>
+        <div className='pay-buttons'>
+          <button className='checkout-button'>CHECKOUT <FontAwesomeIcon icon={faLongArrowAltRight}/></button>
           <div>OR</div>
-          <button>PAYPAL</button>
+          <button className='paypal-button'><img src={PaypalLogo}/><FontAwesomeIcon icon={faLongArrowAltRight}/></button>
         </div>
-        <div>
-          <button>CHECKOUT</button>
-          <div>OR</div>
-          <button>PAYPAL</button>
-        </div>
-        <div>
+        <div className='shopping-announcement'>
           <div><FontAwesomeIcon icon={faCarSide}/> FREE SHIPPING OVER $49 AND FREE RETURNS</div>
+          <br></br>
           <div><FontAwesomeIcon icon={faClock}/> BUY NOW PAY OVER TIME</div>
         </div>
       </div>
-      <div>
-        <div>CONTINUE SHOPPING</div>
-        <div>
-          <button>CHECKOUT</button>
+      <div className='summary-side'>
+        <div className='continue-shopping'>CONTINUE SHOPPING</div>
+        <div className='pay'>
+          <button className='checkout-button'>CHECKOUT <FontAwesomeIcon icon={faLongArrowAltRight}/></button>
           <div>OR</div>
-          <button>PAYPAL</button>
+          <button className='paypal-button'><img src={PaypalLogo}/><FontAwesomeIcon icon={faLongArrowAltRight}/></button>
         </div>
-        <div>
+        <div className='order-summary'>
           <h3><b>ORDER SUMMARY</b></h3>
+          <div class="split">
+            <div>{itemsLength} {itemsLength != 1 ? 'ITEMS' : 'ITEM'}</div>
+            <div>${totalPrice}</div>
+          </div>
+          <div class="split">
+            <div>DELIVERY</div>
+            <div>FREE</div>
+          </div>
+          <div class="split">
+            <div>SALES TAX</div>
+            <div>-</div>
+          </div>
+          <div class="split">
+            <div><b>TOTAL</b></div>
+            <div>${totalPrice}</div>
+          </div>
         </div>
-        <div>
-          <input placeholder='ENTER YOUR PROMO CODE'></input>
+        <div className='promo'>
+          <button>ENTER YOUR PROMO CODE <FontAwesomeIcon icon={faPlus}/></button>
         </div>
       </div>
     </div>
