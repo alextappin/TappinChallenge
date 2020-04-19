f8ea285f96aa4e63ee48a93cc46fa2ef44931c22
Alex Tappin <alextappin@gmail.com>
2020-04-19 03:33:07 -0700

    revert changes

--- src/services/cart.js
+++ src/services/cart.js
@@ -1,10 +1,10 @@
 export default {
   createBasketId: async () => {
-    let response = await fetch('http://cors.io/?https://www.adidas.com/api/checkout/baskets', {
+    let response = await fetch('https://www.adidas.com/api/checkout/baskets', {
       method: 'POST',
     })
     if (response.status === 403) {
-      const errorFix403 = window.open('http://cors.io/?https://www.adidas.com/us/cart','1366002941508','width=10,height=10,left=375,top=330')
+      const errorFix403 = window.open('https://www.adidas.com/us/cart','1366002941508','width=10,height=10,left=375,top=330')
       setTimeout(() => { 
         errorFix403.close()
         location.reload();
@@ -18,13 +18,13 @@ export default {
     return data.basketId
   },
   getBasketGivenId: async (basketId) => {
-    let response = await fetch(`http://cors.io/?https://www.adidas.com/api/checkout/baskets/${basketId}`)
+    let response = await fetch(`https://www.adidas.com/api/checkout/baskets/${basketId}`)
     let data = await response.json()
     console.log(data)
     return data
   },
   addToBasket: async (basketId, productId, quantity) => {
-    await fetch(`http://cors.io/?https://WWW.adidas.com/api/checkout/baskets/${basketId}/items`, {
+    await fetch(`https://WWW.adidas.com/api/checkout/baskets/${basketId}/items`, {
       method: 'POST',
       headers: {
         'content-type': 'application/json; charset=utf-8'
@@ -40,7 +40,7 @@ export default {
     })
   },
   addArrayToBasket: async (basketId, products) => {
-    await fetch(`http://cors.io/?https://WWW.adidas.com/api/checkout/baskets/${basketId}/items`, {
+    await fetch(`https://WWW.adidas.com/api/checkout/baskets/${basketId}/items`, {
       method: 'POST',
       headers: {
         'content-type': 'application/json; charset=utf-8'
@@ -51,7 +51,7 @@ export default {
     })
   },
   updateBasketItem: async (basketId, itemId, item) => {
-    await fetch(`http://cors.io/?https://WWW.adidas.com/api/checkout/baskets/${basketId}/items/${itemId}`, {
+    await fetch(`https://WWW.adidas.com/api/checkout/baskets/${basketId}/items/${itemId}`, {
       method: 'PATCH',
       headers: {
         'content-type': 'application/json; charset=utf-8'
@@ -62,7 +62,7 @@ export default {
     })
   },
   removeBasketItem: async (basketId, itemId) => {
-    await fetch(`http://cors.io/?https://WWW.adidas.com/api/checkout/baskets/${basketId}/items/${itemId}`, {
+    await fetch(`https://WWW.adidas.com/api/checkout/baskets/${basketId}/items/${itemId}`, {
       method: 'DELETE',
       headers: {
         'content-type': 'application/json; charset=utf-8'
